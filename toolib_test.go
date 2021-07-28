884a71de6e1f80edae3da76162df045905605829
scorpiotzh <835598264@qq.com>
2021-07-28 16:56:35 +0800

    add json str

--- toolib_test.go
+++ toolib_test.go
@@ -14,3 +14,23 @@ func TestExitMonitoring(t *testing.T) {
 	})
 	<-exit
 }
+
+func TestJsonString(t *testing.T) {
+	type TestJson struct {
+		A string            `json:"a"`
+		B int               `json:"b"`
+		C []string          `json:"c"`
+		D map[string]string `json:"d"`
+	}
+	var tj TestJson
+	tj.A = "A"
+	tj.B = 1
+	tj.C = []string{"a", "b"}
+	tj.D = map[string]string{
+		"a": "1",
+		"b": "2",
+	}
+	fmt.Println(JsonString(tj))
+	fmt.Println(JsonString(tj.C))
+	fmt.Println(JsonString(tj.D))
+}
